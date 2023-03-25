55c81f54357762b072e2c82ea2e4b3b841383772
HUDASULTANA <125246015+HUDASULTANA@users.noreply.github.com>
2023-03-25 14:28:31 +0530

    Add files via upload

---
+++ home.js
@@ -0,0 +1,20 @@
+.button button{
+    padding:9px 14px;
+    border-radius: 22px;
+    color: aliceblue;
+    background-color:aqua;
+    font-weight: bold;
+    font-size: 21px;
+    margin:0 3px;
+    cursor: pointer;
+}
+.button button:hover{color:aqua;
+    background-color: white;}
+.more{
+    font-size: 22px;
+    font-family: 'IBM Plex Sans', sans-serif;
+    margin-top: 23px;
+}
+.button{
+    margin-top: 34px;
+}
